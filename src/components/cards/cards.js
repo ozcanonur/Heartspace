462a5074ad1813e8654569db1ed02c896e0d1ad9
ozcanonur <ozcanonur10@gmail.com>
2020-11-11 15:01:36 +0000

    Backbone somewhat done

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import classes from './cards.module.scss';
 
 const Cards = () => {
-  return <div></div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.sectionContainer}>
+        <div className={classes.img} />
+        <div className={classes.card}>
+          <div className={classes.cardTitle}>Relationship Strength Quiz</div>
+          <div className={classes.cardText}>
+            Psychology-based relationship assessment will help key areas you can
+            improve in your relationship
+          </div>
+          <button className={classes.button}>Check it out!</button>
+        </div>
+      </div>
+      <div className={classes.sectionContainer}>
+        <div className={classes.img} />
+        <div className={classes.card}>
+          <div className={classes.cardTitle}>About us / Who are we?</div>
+          <div className={classes.cardText}>
+            We'd love to hear from you! Get in touch to Lorem ipsum dolor sit
+            amet consectetur, adipisicing elit. Rerum numquam laboriosam
+            voluptatem consequatur cupiditate voluptas! Lorem ipsum dolor sit,
+            amet consectetur adipisicing elit. Repellat, optio.
+          </div>
+          <button className={classes.button}>Check it out!</button>
+        </div>
+      </div>
+      <div className={classes.sectionContainer}>
+        <div className={classes.img} />
+        <div className={classes.card}>
+          <div className={classes.cardTitle}>Relationship Strength Quiz</div>
+          <div className={classes.cardText}>
+            Psychology-based relationship assessment will help key areas you can
+            improve in your relationship
+          </div>
+          <button className={classes.button}>Check it out!</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import classes from './footer.module.scss';
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.privacy}>Privacy Policy</div>
+      <div className={classes.copyright}>
+        Happy Relationships copyright vs vs.
+      </div>
+      <div className={classes.icon} />
+    </div>
+  );
 };
 
 export default Footer;
