a07314cf7429bdca3935def1e35468468ab032a3
Murat Derya Ozen <muratdozen@gmail.com>
2021-01-04 19:28:25 +0300

    fix

--- src/components/relationshipAssessment/relationshipAssessment.js
+++ src/components/relationshipAssessment/relationshipAssessment.js
@@ -31,7 +31,7 @@ const RelationshipAssessment = () => {
     setTimeout(() => {
       const { positiveAndNegativeScores } = getPositiveAndNegativeScores(formDataSerialized);
 
-      setScores(positiveAndNegativeScores);
+      setPositiveAndNegativeScores(positiveAndNegativeScores);
 
       setIsAssessmentDone(true);
     }, resultScreenTimeoutLength);
