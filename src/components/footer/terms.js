a504465a3a544be060a16efb43e327c125540d69
Murat Derya Ozen <muratdozen@gmail.com>
2021-01-10 13:47:40 +0300

    fix

---
+++ src/components/footer/terms.js
@@ -0,0 +1,147 @@
+const terms = `# Happy Relationships is a movement aiming to make mental wellbeing more accessible.
+
+This privacy policy will explain how Happy Relationships uses the personal data we collect from you when you use our website.
+
+<br />
+
+## Topics:
+
+<br />
+
+- What data do we collect?
+- How do we collect your data?
+- How will we use your data?
+- How do we store your data?
+- Marketing
+- What are your data protection rights?
+- What are cookies?
+- How do we use cookies?
+- What types of cookies do we use?
+- How to manage your cookies
+- Privacy policies of other websites
+- Changes to our privacy policy
+- How to contact us
+- How to contact the appropriate authorities
+
+<br />
+
+## What data do we collect?
+
+<br />
+
+### Happy Relationships collects the following data:
+
+<br />
+
+- Email address.
+- Survey responses (voluntary)
+
+<br />
+
+## How do we collect your data?
+
+<br />
+
+You directly provide Happy Relationships with most of the data we collect. We collect data and process data when you:
+
+<br />
+
+- Voluntarily complete our “contact us" form,
+- Voluntarily complete our survey
+  Happy Relationships uses third party libraries and because of this, they will collect data from you according to their privacy policy. Happy Relationships does not have access to your data collected by those third parties.
+  How will we use your data?
+  Happy Relationships collects your data so that we can:
+- Be notified of your interest in our product and reach out to you when our product is ready.
+  Happy Relationships will not use your email address in any other way.
+
+  <br />
+
+  ## How do we store your data?
+<br />
+
+Happy Relationships securely stores your data using Google's secure infrastructure is also protected via a two-phase authentication mechanism.
+Happy Relationships will keep your email address data for 6 months. Once this time period has expired, we will delete your data using our secure infrastructure.
+
+<br />
+
+## Marketing
+
+<br />
+
+Happy Relationships will only contact you to let you know when our product is ready for use.
+
+<br />
+
+## What are your data protection rights?
+
+<br />
+
+Happy Relationships would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:
+The right to access – You have the right to request Happy Relationships for copies of your personal data. We may charge you a small fee for this service.
+The right to rectification – You have the right to request that Happy Relationships correct any information you believe is inaccurate. You also have the right to request Our Company to complete the information you believe is incomplete.
+The right to erasure – You have the right to request that Happy Relationships erase your personal data, under certain conditions.
+The right to restrict processing – You have the right to request that Happy Relationships restrict the processing of your personal data, under certain conditions.
+The right to object to processing – You have the right to object to Happy Relationships' processing of your personal data, under certain conditions.
+The right to data portability – You have the right to request that Happy Relationships transfer the data that we have collected to another organization, or directly to you, under certain conditions.
+If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us using the contact info at the end of this document.
+
+<br />
+
+## Cookies
+
+<br />
+
+Cookies are text files placed on your computer to collect standard Internet log information and visitor behavior information. When you visit our websites, we will not collect information from you via cookies.
+For further information, visit allaboutcookies.org.
+
+<br />
+
+### How do we use cookies?
+
+<br />
+
+- We don't.
+
+<br />
+
+## How to manage cookies
+
+<br />
+
+You can set your browser not to accept cookies, and the above website tells you how to remove cookies from your browser. However, in a few cases, some of our website features may not function as a result.
+
+<br />
+
+## Privacy policies of other websites
+
+<br />
+
+Our privacy policy applies only to our website, so if you click on a link to another website, you should read their privacy policy.
+
+<br />
+
+## Changes to our privacy policy
+
+<br />
+
+Happy Relationships keeps its privacy policy under regular review and places any updates on this web page. This privacy policy was last updated on 14 November 2020.
+
+<br />
+
+## How to contact the appropriate authority
+
+<br />
+
+Should you wish to report a complaint or if you feel that Happy Relationships has not addressed your concern in a satisfactory manner, you may contact the Information Commissioner’s Office.
+https://ico.org.uk/global/contact-us/
+
+<br />
+
+## How to contact us
+
+<br />
+
+If you have any questions about Happy Relationships’ privacy policy, the data we hold on you, or you would like to exercise one of your data protection rights, please do not hesitate to contact us using the “Contact Us” form on our website.
+`;
+
+export default terms;
